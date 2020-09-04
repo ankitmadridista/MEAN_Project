23030d3006870b3aa4c598dca521a3856a9c4ab6
ankitmadridista <ankit.suravkar4@gmail.com>
2020-09-04 23:40:12 +0530

    contactus.done

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent implements OnInit {
+  public imgsrc = 'assets/contact.svg';
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
